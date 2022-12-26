eef1e8a88d72bb55781df89876f17e8464d78be7
Kalvium <109285740+Kalvium-Program@users.noreply.github.com>
2022-12-26 10:45:43 +0530

    Create FoodData.js

---
+++ resources/FoodData.js
@@ -0,0 +1,32 @@
+export default [{
+  id:0,
+  img:"https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/pizza-react.jpg",
+  name:"pizza",
+  cal:266,
+},
+{
+  id:1,
+  img:"https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/applie-react.jpeg",
+  name:"apple",
+  cal:52,
+},
+{
+  id:3,
+  img:"https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/orange-react.jpeg",
+  name:"oranges",
+  cal:47,
+},
+{
+  id:4,
+  img:"https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/almonds-react.jpeg",
+  name:"almonds",
+  cal:7,
+},
+{
+  id:5,
+  img:"https://s3.ap-south-1.amazonaws.com/kalvi-education.github.io/burger-react.jpeg",
+  name:"burger",
+  cal:295,
+},
+
+]
